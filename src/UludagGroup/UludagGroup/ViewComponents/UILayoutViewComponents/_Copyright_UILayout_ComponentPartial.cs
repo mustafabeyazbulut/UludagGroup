2ec098621f66c4568beaaf8d50e01a2a290c7446
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2025-04-09 21:32:12 +0300

    initial project

---
+++ src/UludagGroup/UludagGroup/ViewComponents/UILayoutViewComponents/_Copyright_UILayout_ComponentPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UludagGroup.ViewComponents.UILayoutViewComponents
+{
+    public class _Copyright_UILayout_ComponentPartial : ViewComponent
+    {
+        public _Copyright_UILayout_ComponentPartial()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/UludagGroup/UludagGroup/ViewComponents/UILayoutViewComponents/_Head_UILayout_ComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlpayMakina.ViewComponents.UILayoutViewComponents
+{
+    public class _Head_UILayout_ComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
