d6f03d79691f3c3365b4f1479c441a9ed258fb9b
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2025-04-10 14:00:24 +0300

    .

---
+++ UludagGroup/ViewComponents/UILayoutViewComponents/_Script_UILayout_ComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlpayMakina.ViewComponents.UILayoutViewComponents
+{
+    public class _Script_UILayout_ComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
